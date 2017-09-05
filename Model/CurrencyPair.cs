dbb2333eed8c9cd635e6d3476d13a76109599c26
Michael <michael@gatecoin.com>
2017-09-05 17:47:55 +0800

    DTO: Add CurrencyPair new attributes

--- Model/CurrencyPair.cs
+++ Model/CurrencyPair.cs
@@ -9,8 +9,12 @@ namespace GatecoinServiceInterface.Model{
 [Serializable]
 public class CurrencyPair 
 {
-public System.String Name {get; set; } 
-public System.String TradingCode {get; set; } 
+public System.String Name {get; set; }
+public System.String TradingCode {get; set; }
+public System.String BaseCurrency {get; set; }
+public System.String QuoteCurrency {get; set; }
+public System.String DisplayName {get; set; }
+public System.Int64 PriceDecimalPlaces {get; set; }
 }
 }
 
