f6f32749148668538eb15dec513537095d6dc67f
sukhendughosh <sukhendu@gatecoin.com>
2018-08-24 14:58:03 +0800

    Updated AcceptTermsAndConditions

--- Request/AcceptTermsAndConditions.cs
+++ Request/AcceptTermsAndConditions.cs
@@ -8,19 +8,13 @@ using GatecoinServiceInterface.Response;
 
 namespace GatecoinServiceInterface.Request
 {
-    [Route("/preview/Account/TermsAndConditionsStatus", "POST", Summary = @"Accept T&C", Notes = @"")]
-    public class PostTermsAndConditionResponse : IReturn<GetTermsAndConditionsResponse>
-    {
-        [ApiMember(Name = "AcceptedTermsAndConditions", Description = "Whether or not the logged in user has accepted the T&C", ParameterType = "query", DataType = "bool", IsRequired = false)]
-        public Boolean AcceptedTermsAndConditions { get; set; }
-    }
+[Route("/preview/Account/TermsAndConditionsStatus", "POST", Summary = @"Accept T&C", Notes = @"")]
+public class AcceptTermsAndCondition : IReturn<CommonResponse>
+{
+    [ApiMember(Name = "AcceptedTermsAndConditions", Description = "Whether or not the logged in user has accepted the T&C", ParameterType = "query", DataType = "bool", IsRequired = false)]
+    public Boolean AcceptedTermsAndConditions { get; set; }
+}
 
-    [Route("/preview/Account/TermsAndConditionsStatus", "POST", Summary = @"Accept T&C", Notes = @"")]
-    public class PostTermsAndConditionResponseAsNull : IReturn<GetTermsAndConditionsResponse>
-    {
-        [ApiMember(Name = "AcceptedTermsAndConditions", Description = "Whether or not the logged in user has accepted the T&C", ParameterType = "query", DataType = "string", IsRequired = false)]
-        public String AcceptedTermsAndConditions { get; set; }
-    }
 }
 
 
