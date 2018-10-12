c11331c4be74b4a9e08cfdb72783939c271bf94a
sukhendughosh <sukhendu@gatecoin.com>
2018-10-12 16:20:34 +0800

    Updated ServiceClient to ignore certificates Added Model for BankTransfer Added Request response for FiatDeposit Accounts

---
+++ Request/GetFiatDepositsInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using ServiceStack.Common;
+using ServiceStack.ServiceHost;
+using ServiceStack.Common.Web;
+using GatecoinServiceInterface.Response;
+namespace GatecoinServiceInterface.Request
+{
+    [Route("/Bank/Deposits", "GET", Summary = @"Gets Fiat Deposits Info", Notes = @"")]
+    public class GetFiatDepositsInfo : IReturn<FiatDepositsResponse>
+    {
+    }
+}
---
+++ Response/FiatDepositsResponse.cs
@@ -0,0 +1,15 @@
+﻿using ServiceStack.ServiceInterface.ServiceModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GatecoinServiceInterface.Model;
+namespace GatecoinServiceInterface.Response
+{
+    public class FiatDepositsResponse : CommonResponse
+    {
+        public String DepositReferenceCode { get; set; }
+        public List<FiatDepositInfo> Accounts { get; set; }
+    }
+}
